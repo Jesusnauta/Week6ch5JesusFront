770231e2e30a4a988b0e76f9cc4c97abdfd16c65
Jesus <jesus.cassano.2006@gmail.com>
2023-02-26 23:21:34 +0100

    ✅ Add fix

--- src/features/mocks/beers.mock.ts
+++ src/features/mocks/beers.mock.ts
@@ -3,7 +3,7 @@ export const MOCK_BEERS = [
   { id: 2, name: "Mahou", price: 3.5 },
   { id: 3, name: "Estrella Galicia", price: 3 },
   { id: 4, name: "Modelo", price: 5 },
-  { id: 5, name: "Stella Artois", price: 5 },
+  { id: 5, name: "Stella Artois", price: 4.5 },
   {},
   {},
   {},
